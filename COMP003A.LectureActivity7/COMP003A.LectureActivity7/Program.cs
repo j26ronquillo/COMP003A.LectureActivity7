c003fce8ac74f2e29f20975d9f0bded1032477a6
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-24 18:40:38 -0800

    finished the array and list step

---
+++ COMP003A.LectureActivity7/COMP003A.LectureActivity7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace COMP003A.LectureActivity7
+{
+	internal class Program
+	{ 
+		static void Main(string[] args)
+		{
+			
+			int[] scores = {9, 23, 36, 42, 59 };
+
+			for (int i = 0; i < scores.Length; i++)
+			{
+				Console.WriteLine(scores[i]);
+			}
+			
+			
+			List<string> names = new List<string>();
+			names.Add("Josh");
+			names.Add("Steph");
+			names.Add("Monish");
+
+			foreach (string name in names)
+			{
+				Console.WriteLine(name);
+			}
+		}
+	}
+}
